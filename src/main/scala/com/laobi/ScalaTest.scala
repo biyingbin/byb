feec54789d9ad537637160701b300b0caa23813c
laobi <biyingbin@wifipix.com>
2020-04-19 22:39:40 +0800

    readme

--- src/main/scala/com/laobi/ScalaTest.scala
+++ src/main/scala/com/laobi/ScalaTest.scala
@@ -10,25 +10,6 @@ object ScalaTest {
     println("test scala4")
     println("test scala5")
 
-    if(true)
-      println("test scala6")
-    println("test scala7")
-    println("test scala8")
-    println("test scala9")
-    println("test scala10")
-
-    println("test scala11")
-    println("test scala12")
-    println("test scala13")
-    if(true)
-      println("test scala14")
-
-    println("test scala15")
-    println("test scala16")
-    println("test scala17")
-
-
-
   }
 
   def test(): Unit = {
