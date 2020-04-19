e646bd98a8cf246a33aa38e090a9f66918cc0bcd
laobi <biyingbin@wifipix.com>
2020-04-19 15:15:18 +0800

    asdadasd

--- src/main/scala/com/laobi/ScalaTest.scala
+++ src/main/scala/com/laobi/ScalaTest.scala
@@ -5,20 +5,17 @@ object ScalaTest {
   def main(args: Array[String]): Unit = {
 
     println("test scala1")
-    println("test scala2")
-    println("test scala3")
-    println("test scala4")
     println("test scala5")
-    println("test scala5")
-    println("test scala5")
-    println("test scala6")
+    if(true)
+      println("test scala6")
     println("test scala7")
     println("test scala8")
     println("test scala9")
     println("test scala10")
     println("test scala12")
     println("test scala13")
-    println("test scala14")
+    if(true)
+      println("test scala14")
 
 
   }
